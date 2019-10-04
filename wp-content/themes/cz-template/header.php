b86ab4d366693a4158874cdd9a7c4ddfad0bee01
Leonardo Santana <hi@leonsantana.com>
2019-10-04 15:59:05 -0400

    added dynamic transitional logo

--- wp-content/themes/cz-template/header.php
+++ wp-content/themes/cz-template/header.php
@@ -15,6 +15,8 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
+    <!-- Animated logo  -->
+<?php get_template_part( 'template-parts/cz-brand' )?>
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
 
 		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
---
+++ wp-content/themes/cz-template/template-parts/cz-brand.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Displays the custom transitional logo for ChilZone
+ *
+ * @package WordPress
+ * @subpackage cz-template
+ * @since 1.0.0
+ */
+?>
+<div class="logo-container">
+    <img src="<?php bloginfo('stylesheet_directory')?>/images/CZ-Logo-ColorSteeps-01.png" alt="main-chilzone-logo-heartbulb" />
+ </div>
