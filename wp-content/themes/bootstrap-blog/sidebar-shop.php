fd5d32aaa824e385af4cf9587f8a71e52736405c
Leonardo Santana <hi@leonsantana.com>
2019-11-26 15:18:52 -0500

    removed bootstrap theme

--- wp-content/themes/bootstrap-blog/sidebar-shop.php
+++
@@ -1,3 +0,0 @@
-<?php if ( is_active_sidebar( 'woocommerce_sidebar' ) ) : ?>
-    <?php dynamic_sidebar( 'woocommerce_sidebar' ); ?>
-<?php endif;
--- wp-content/themes/bootstrap-blog/template-parts/home-sections/instagram.php
+++
@@ -1,9 +0,0 @@
-<?php if ( is_active_sidebar( 'instagram' ) ) : ?>
-	<div class="widget-instagram clearfix">
-		<?php dynamic_sidebar( 'instagram' ); ?>
-	</div>
-<?php endif; ?>
-
-
-
-
