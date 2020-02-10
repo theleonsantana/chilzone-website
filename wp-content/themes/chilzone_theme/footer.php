c95bd2e0571aa7acedb0d428892357480c64409c
Leonardo Santana <hi@leonsantana.com>
2020-02-10 17:00:00 -0500

    started working on the main footer for the chilZone website

--- wp-content/themes/chilzone_theme/footer.php
+++ wp-content/themes/chilzone_theme/footer.php
@@ -1,20 +1,37 @@
-      <!-- footer -->
-      <div class="bottom-transition"></div>
-			<footer class="footer" role="contentinfo">
+  
+<div class="bottom-transition"></div>
+  <footer class="footer" role="contentinfo">
+  
+    <div class="container">
+      <div class="row">
+        
+        <div class="col-sm-6">
+          <img src="<?php bloginfo('stylesheet_directory')?>/images/ChilZone-logo.png" />
+        </div>
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
-				</p>
-				<!-- /copyright -->
+        <div class="col-sm-6">
+          <div class="branding-footer">
+            <img src="<?php bloginfo('stylesheet_directory')?>/images/Monte-logo.png"/>
+            <img src="<?php bloginfo('stylesheet_directory')?>/images/EA-logo.png" />
+          </div>
+          <?php
+          echo do_shortcode( '[ninja_form id=1]' );
+          ?>
+        </div>       
+      </div>
+      <div class="row">
 
-			</footer>
-			<!-- /footer -->
+        <div class="col-sm-12">
+        <!-- copyright -->
+        <p class="copyright">&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.</p>
+        <!-- /copyright -->
+      </div>
 
-		</div>
-		<!-- /.container-fluid -->
-
-		<?php wp_footer(); ?>
-
-	</body>
+      </div>
+    </div>
+  
+  </footer>
+  
+  <?php wp_footer(); ?>
+  </body>
 </html>
