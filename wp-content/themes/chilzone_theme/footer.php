f01200cb2e984601f232e5bd9f0c4500d6a19db6
Leonardo Santana <hi@leonsantana.com>
2020-02-12 13:28:55 -0500

    added styles for the footer, ready to push first round to staging

--- wp-content/themes/chilzone_theme/footer.php
+++ wp-content/themes/chilzone_theme/footer.php
@@ -5,7 +5,7 @@
     <div class="container">
       <div class="row">
         
-        <div class="col-sm-6">
+        <div class="col-sm-6 footer-logo-container">
           <img src="<?php bloginfo('stylesheet_directory')?>/images/ChilZone-logo.png" />
         </div>
 
@@ -14,6 +14,7 @@
             <img src="<?php bloginfo('stylesheet_directory')?>/images/Monte-logo.png"/>
             <img src="<?php bloginfo('stylesheet_directory')?>/images/EA-logo.png" />
           </div>
+          <h3 class="footer-heading">Contact Us!</h3>
           <?php
           echo do_shortcode( '[ninja_form id=1]' );
           ?>
@@ -31,6 +32,16 @@
     </div>
   
   </footer>
+  <!-- nav -->
+  <nav>
+      <?php
+      wp_nav_menu( array(
+          'theme_location'  => 'secondary',
+          'menu_class'     => 'footer-menu nav justify-content-center',
+      ))
+      ?>
+  </nav>
+  <!-- /nav -->
   
   <?php wp_footer(); ?>
   </body>
